394269ef287326ff61660a8b6e540c6d1e456b56
Allan Guerreiro Carneiro <allanguerreiro@gmail.com>
2016-11-28 14:48:32 -0200

    Configuracoes de protocolo ssl

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # RestServiceSpringBoot
+
+
+-- No linux
+openssl req -newkey rsa:2048 -x509 -keyout /home/allan/ssl/tomcat/cakey.pem -out /home/allan/ssl/tomcat/cacert.pem -days 3650
+
+openssl pkcs12 -export -in /home/allan/ssl/tomcat/cacert.pem -inkey /home/allan/ssl/tomcat/cakey.pem -out /home/allan/ssl/tomcat/identity.p12 -name "tomcat"
+
+keytool -import -file /home/allan/ssl/tomcat/cacert.pem -keystore /home/allan/ssl/tomcat/trust.jks -storepass changeit
+
+
+-- Tomcat
+<Connector port="8443"
+			maxHttpHeaderSize="8192" SSLEnabled="true"
+			maxThreads="150" minSpareThreads="25"
+			enableLookups="false" disableUploadTimeout="true"
+			acceptCount="100" scheme="https" secure="true"
+			clientAuth="false" sslProtocol="TLS" useBodyEncodingForURI="true"
+			keystoreFile="/home/allan/ssl/tomcat/identity.p12"
+			keystorePass="changeit" keystoreType="PKCS12" keyAlias="tomcat" />
--- src/main/java/org/rest/Application.java
+++ src/main/java/org/rest/Application.java
@@ -1,11 +1,17 @@
 package org.rest;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.rest.configuration.JerseyInitialization;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -33,5 +39,33 @@ public class Application extends SpringBootServletInitializer {
         ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/*");
         registration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, JerseyInitialization.class.getName());
         return registration;
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
     }
 }
